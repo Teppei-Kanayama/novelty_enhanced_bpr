4c2a660d97c606affcecc4bff2215b5f7e99f2f3
Teppei Kanayama <teppei@Kanayama-2.local>
2020-02-24 15:32:59 +0900

    many users

--- novelty_enhanced_bpr/visualize_psudo_data.py
+++ novelty_enhanced_bpr/visualize_psudo_data.py
@@ -20,7 +20,7 @@ class VisualizePsudoData(gokart.TaskOnKart):
 
         for i, c in enumerate(['r', 'g', 'b', 'c']):
             self.draw_scatter(item_embed[item_embed['item_type'] == i]['item_vector'].values, c)
-        self.draw_scatter(user_embed['user_vector'].values, 'black', size=5)
+        self.draw_scatter(user_embed['user_vector'].values, 'black', alpha=0.07, size=5)
         plt.savefig('resources/scatter.png')
 
         ctr = clicks.groupby('item_type', as_index=False).agg({'click': 'mean'}).rename(columns={'click': 'CTR'})
@@ -31,8 +31,9 @@ class VisualizePsudoData(gokart.TaskOnKart):
         self.dump('this is dummy output')
 
     @staticmethod
-    def draw_scatter(embed, color, size=20):
+    def draw_scatter(embed, color, size=20, alpha=1.0):
         embed_matrix = np.stack(embed)
         x = embed_matrix[:, 0]
         y = embed_matrix[:, 1]
-        plt.scatter(x, y, c=color, s=size)
+        plt.scatter(x, y, c=color, s=size, alpha=alpha)
+
