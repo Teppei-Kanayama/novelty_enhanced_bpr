6e197d20e14389b99a0546b95bf43749130d6642
Teppei Kanayama <teppei@Kanayama-2.local>
2020-02-27 22:07:35 +0900

    visualize

--- novelty_enhanced_bpr/visualize_psudo_data.py
+++ novelty_enhanced_bpr/visualize_psudo_data.py
@@ -19,10 +19,15 @@ class VisualizePsudoData(gokart.TaskOnKart):
         user_embed = self.load('user_embed')
         clicks = self.load('data')['clicks_train']
 
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
         for i, c in enumerate(['r', 'g', 'b', 'c']):
-            self.draw_scatter(item_embed[item_embed['item_type'] == i]['item_vector'].values, c)
-        self.draw_scatter(user_embed['user_vector'].values, 'black', alpha=0.07, size=5)
-        plt.savefig('resources/scatter.png')
+            self.draw_scatter(ax, item_embed[item_embed['item_type'] == i]['item_vector'].values, c, f'item_type: {i}')
+        self.draw_scatter(ax, user_embed['user_vector'].values, 'black', 'user', alpha=0.07, size=5)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.legend(loc='upper left')
+        fig.savefig('resources/scatter.png')
 
         sum_clicks = clicks.groupby('item_id', as_index=False).agg({'click': 'sum'}).rename(columns={'click': 'sum_clicks'})
         clicks = pd.merge(clicks, sum_clicks, on='item_id', how='inner')
@@ -35,9 +40,9 @@ class VisualizePsudoData(gokart.TaskOnKart):
         self.dump('this is dummy output')
 
     @staticmethod
-    def draw_scatter(embed, color, size=20, alpha=1.0):
+    def draw_scatter(ax, embed, color, label, size=20, alpha=1.0):
         embed_matrix = np.stack(embed)
         x = embed_matrix[:, 0]
         y = embed_matrix[:, 1]
-        plt.scatter(x, y, c=color, s=size, alpha=alpha)
+        ax.scatter(x, y, c=color, s=size, alpha=alpha, label=label)
 
